afde2362d078e8bdca5aa7d59b0d95587604c9c8
Modasser Hasan <manvstech2.0@gmail.com>
2024-10-14 20:49:21 +0600

    Basic initialization done

--- server-side/app.py
+++ server-side/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
 
-# App
+load_dotenv()
+
+# App & Database Initialization
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("MONGODB_URI")
+db = PyMongo(app).db
 
 
 @app.route("/")
 def home():
+    db.test.insert_one({"success": True})
     return {"success": True}
 
 
